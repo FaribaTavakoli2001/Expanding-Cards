9634506af28b1e1508114221054851e89ca49cde
FaribaTavakoli2001 <117892836+FaribaTavakoli2001@users.noreply.github.com>
2023-07-25 21:27:55 +0330

    with-mouse-in-show-image

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 const panels = document.querySelectorAll('.panel');
 
 panels.forEach(panel => {
-    panel.addEventListener('click', () => {
+    panel.addEventListener('mouseenter', () => {
         removeactiveClasses();
         panel.classList.add('active');
     }
